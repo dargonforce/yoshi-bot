713103c7e912ae92517ab93a1919a635c32762b9
Tim <yoshiclub@airmail.cc>
2019-05-10 11:47:42 -0400

    Refactored some stuff

--- bot.py
+++ bot.py
@@ -1,47 +1,13 @@
 #!/usr/bin/env python3
 import discord
 import markopy.marko
-
-class And(object):
-    def __init__(self, lhs, rhs):
-        self.lhs = lhs
-        self.rhs = rhs
-    def __call__(self, string: str) -> bool:
-        return self.lhs(string) and self.rhs(string)
-
-class Not(object):
-    def __init__(self, pred):
-        self.pred = pred
-    def __call__(self, string: str) -> bool:
-        return not self.pred(string)
+from functor import Contains, StartsWith, And, Not
 
 class SendResponse(object):
     def __init__(self, rspns: str):
         self.response = rspns
     async def __call__(self, message: discord.Message):
         await message.channel.send(self.response)
-
-class Contains(object):
-    def __init__(self, txt: str, ignore_case: bool = True):
-        self.text = txt
-        if ignore_case:
-            self.text = self.text.lower()
-        self.ignore_case = ignore_case
-    def __call__(self, string:str) -> bool:
-        if self.ignore_case:
-            string = string.lower()
-        return self.text in string
-
-class StartsWith(object):
-    def __init__(self, txt: str, ignore_case: bool = True):
-        self.text = txt
-        if ignore_case:
-            self.text = self.text.lower()
-        self.ignore_case = ignore_case
-    def __call__(self, string: str) -> bool:
-        if self.ignore_case:
-            string = string.lower()
-        return string.startswith(self.text)
 
 with open('strings.txt') as file:
     strings = {}
@@ -59,17 +25,14 @@ commands = [
     (StartsWith('>tfwnogf'), SendResponse(strings['tfwnogf']))
 ]
 
-async def handle_command(message):
-    for command in commands:
-        if command[0](message.content):
-            await command[1](message)
-
 class BotClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
     async def on_message(self, message):
         if message.author != self.user:
-            await handle_command(message)
+            for command in commands:
+                if command[0](message.content):
+                    await command[1](message)
 
 def start_client():
     client = BotClient()
---
+++ functor.py
@@ -0,0 +1,39 @@
+class Contains(object):
+    def __init__(self, txt: str, ignore_case: bool = True):
+        self.text = txt
+        if ignore_case:
+            self.text = self.text.lower()
+        self.ignore_case = ignore_case
+    def __call__(self, string:str) -> bool:
+        if self.ignore_case:
+            string = string.lower()
+        return self.text in string
+
+class StartsWith(object):
+    def __init__(self, txt: str, ignore_case: bool = True):
+        self.text = txt
+        if ignore_case:
+            self.text = self.text.lower()
+        self.ignore_case = ignore_case
+    def __call__(self, string: str) -> bool:
+        if self.ignore_case:
+            string = string.lower()
+        return string.startswith(self.text)
+
+class And(object):
+    def __init__(self, *params):
+    	self.params = params
+    def __call__(self, string: str) -> bool:
+    	return all(map(lambda x: x(string), self.params))
+
+class Or(object):
+    def __init__(self, *params):
+    	self.params = params
+    def __call__(self, string: str) -> bool:
+    	return any(map(lambda x: x(string), self.params))
+
+class Not(object):
+    def __init__(self, pred):
+        self.pred = pred
+    def __call__(self, string: str) -> bool:
+        return not self.pred(string)
