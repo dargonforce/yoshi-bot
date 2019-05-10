a30fb88dd3f4b4928e55a623bcda138108cbdff6
Tim <yoshiclub@airmail.cc>
2019-05-09 20:17:38 -0400

    Added don't uwu

--- bot.py
+++ bot.py
@@ -2,26 +2,27 @@
 import discord
 
 with open('strings.txt') as file:
-	strings = {}
-	for line in  file:
-		key, value = line.partition('=')[::2]
-		strings[key.strip()] = value
+    strings = {}
+    for line in  file:
+        key, value = line.partition('=')[::2]
+        strings[key.strip()] = value
 
 with open('auth.txt') as file:
     auth = file.readline().strip(' \n\r')
 
 class BotClient(discord.Client):
-	async def on_ready(self):
-		print('Logged on as', self.user)
-
-	async def on_message(self, message):
-		if message.author != self.user:
-			if 'yoshi' in message.content.lower():
-				await message.channel.send(strings['yoshi'])
-			elif message.content.lower() == '>tfwnogf':
-				await message.channel.send(strings['tfwnogf'])
-			elif message.content.lower().startswith('>tfw'):
-				await message.channel.send(strings['tfw'])
+    async def on_ready(self):
+        print('Logged on as', self.user)
+    async def on_message(self, message):
+        if message.author != self.user:
+            if 'yoshi' in message.content.lower():
+                await message.channel.send(strings['yoshi'])
+            elif message.content.lower() == '>tfwnogf':
+                await message.channel.send(strings['tfwnogf'])
+            elif message.content.lower().startswith('>tfw'):
+                await message.channel.send(strings['tfw'])
+            elif 'uwu' in message.content.lower():
+                await message.channel.send(strings['uwu'])
 
 client = BotClient()
 client.run(auth)
